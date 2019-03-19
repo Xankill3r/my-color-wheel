dc8aaad85503e757123414c078fa66131f5b5694
Ujwal Kumar <ujwal.kumar@immersiveartsstudio.com>
2019-03-20 00:06:01 +0530

    User can specify 6 colors - Default are Yellow, Magenta, Cyan, Red, Blue, Green - Modified layout of page - Made stylesheet external

--- my-color-wheel.js
+++ my-color-wheel.js
@@ -1,39 +1,39 @@
-var color1, color2, color3, grey;
+var color1, color2, color3, color4, color5, color6, grey;
 var colorWheelCtx;
 var diameter = 512;
 
 function updateColorWheel() {
     colorWheelCtx.clearRect(0, 0, diameter, diameter);
-    var c1 = color1.value;
-    var c2 = color2.value;
-    var c3 = color3.value;
-    var g = grey.value;
-    // Primaries
-    drawColorString(colorWheelCtx, c1, g, 0);
-    drawColorString(colorWheelCtx, c2, g, 120);
-    drawColorString(colorWheelCtx, c3, g, 240);
+    var y = color1.value;
+    var m = color2.value;
+    var c = color3.value;
+    var r = color4.value;
+    var g = color5.value;
+    var b = color6.value;
+    var neutral = grey.value;
+    // CMY
+    drawColorString(colorWheelCtx, y, neutral, 0);
+    drawColorString(colorWheelCtx, m, neutral, 120);
+    drawColorString(colorWheelCtx, c, neutral, 240);
+    // RGB
+    drawColorString(colorWheelCtx, r, neutral, 60);
+    drawColorString(colorWheelCtx, b, neutral, 180);
+    drawColorString(colorWheelCtx, g, neutral, 300);
     // Secondaries
-    var c12 = chroma.mix(c1, c2, 0.5, "lch").hex();
-    drawColorString(colorWheelCtx, c12, g, 60);
-    var c23 = chroma.mix(c2, c3, 0.5, "lch").hex();
-    drawColorString(colorWheelCtx, c23, g, 180);
-    var c31 = chroma.mix(c3, c1, 0.5, "lch").hex();
-    drawColorString(colorWheelCtx, c31, g, 300);
-    // Tertiaries
-    var c112 = chroma.mix(c1, c12, 0.5, "lch").hex();
-    drawColorString(colorWheelCtx, c112, g, 30);
-    var c122 = chroma.mix(c12, c2, 0.5, "lch").hex();
-    drawColorString(colorWheelCtx, c122, g, 90);
-    var c223 = chroma.mix(c2, c23, 0.5, "lch").hex();
-    drawColorString(colorWheelCtx, c223, g, 150);
-    var c233 = chroma.mix(c23, c3, 0.5, "lch").hex();
-    drawColorString(colorWheelCtx, c233, g, 210);
-    var c331 = chroma.mix(c3, c31, 0.5, "lch").hex();
-    drawColorString(colorWheelCtx, c331, g, 270);
-    var c311 = chroma.mix(c31, c1, 0.5, "lch").hex();
-    drawColorString(colorWheelCtx, c311, g, 330);
+    var yr = chroma.mix(y, r, 0.5, "lch").hex();
+    drawColorString(colorWheelCtx, yr, neutral, 30);
+    var rm = chroma.mix(r, m, 0.5, "lch").hex();
+    drawColorString(colorWheelCtx, rm, neutral, 90);
+    var mb = chroma.mix(m, b, 0.5, "lch").hex();
+    drawColorString(colorWheelCtx, mb, neutral, 150);
+    var bc = chroma.mix(b, c, 0.5, "lch").hex();
+    drawColorString(colorWheelCtx, bc, neutral, 210);
+    var cg = chroma.mix(c, g, 0.5, "lch").hex();
+    drawColorString(colorWheelCtx, cg, neutral, 270);
+    var gy = chroma.mix(g, y, 0.5, "lch").hex();
+    drawColorString(colorWheelCtx, gy, neutral, 330);
     // Central grey
-    colorWheelCtx.fillStyle = g;
+    colorWheelCtx.fillStyle = neutral;
     colorWheelCtx.beginPath();
     colorWheelCtx.moveTo(diameter / 2, diameter / 2);
     colorWheelCtx.arc(diameter / 2, diameter / 2, (diameter * 0.9 / 20), 0, Math.PI * 2, true);
@@ -64,7 +64,10 @@ function init() {
     color1 = document.getElementById("c1");
     color2 = document.getElementById("c2");
     color3 = document.getElementById("c3");
-    grey = document.getElementById("c4");
+    color4 = document.getElementById("c4");
+    color5 = document.getElementById("c5");
+    color6 = document.getElementById("c6");
+    grey = document.getElementById("grey");
 
     color1.addEventListener("change", function() { updateColorWheel(); }, false);
     color2.addEventListener("change", function() { updateColorWheel(); }, false);
